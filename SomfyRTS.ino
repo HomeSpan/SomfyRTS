c2947cbbeb70cb5d0523e985f837b4ec26a76180
Gregg <homespan@icloud.com>
2020-12-06 17:42:28 -0600

    Update SomfyRTS.ino

--- SomfyRTS.ino
+++ SomfyRTS.ino
@@ -3,7 +3,7 @@
  *  
  *  Copyright (c) 2020 Gregg E. Berman
  *  
- *  https://github.com/HomeSpan/HomeSpan
+ *  https://github.com/HomeSpan/SomfyRTS
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
